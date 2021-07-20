e6ddc4ff0165552a381cc951280652bdb09d3c13
Zaneta Mietka <z.mietka@gmail.com>
2021-07-20 11:53:34 +0200

    Add prompt

--- js/script.js
+++ js/script.js
@@ -21,3 +21,26 @@ else if(computerMove == '2'){
   }
   
   printMessage('Twój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+else if(computerMove == '2'){
+    computerMove = 'papier';
+  }
+  
+  printMessage('Twój ruch to: ' + playerMove);
+
+} else if playerMove == '3'){
+    computerMove = 'nozyce';
+  }
+  
+  printMessage('Twój ruch to: ' + computerMove);
