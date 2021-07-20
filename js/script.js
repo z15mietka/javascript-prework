8eeb1443bfbd7bd8ac2134f09c715362de59f4d1
Zaneta Mietka <z.mietka@gmail.com>
2021-07-20 10:18:58 +0200

    Add first script in script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+
+let playerMove = `papier`;
+
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
