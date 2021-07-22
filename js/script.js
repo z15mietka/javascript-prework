cd2a6a6579a813d71a6699a02ca7b513d3dc3aae
Zaneta Mietka <z.mietka@gmail.com>
2021-07-22 09:46:27 +0200

    Add game result

--- js/script.js
+++ js/script.js
@@ -55,3 +55,79 @@ printMessage('Twój ruch to: ' + computerMove);
 } else if(computerMove == '1'){
     computerMove = 'kamien';
   }
+
+
+
+
+
+
+
+
+  let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+else if(computerMove == '2'){
+    computerMove = 'papier';
+  }
+
+printMessage('Twój ruch to: ' + playerMove);
+
+} else if computerMove == '3'){
+    computerMove = 'nozyce';
+  }
+printMessage('Twój ruch to: ' + computerMove);
+  else if(playerMove == '2'){
+      playerMove = 'papier';
+    }
+  
+printMessage('Twój ruch to: ' + computerMove);
+  
+  } else if playerMove == '3'){
+    playerMove = 'nozyce';
+    }
+
+
+
+
+    if( computerMove == 'kamień' && playerMove == 'papier'){
+        printMessage('Ty wygrywasz!');
+      }
+    if( computerMove == 'nozyce' && playerMove == 'papier'){
+        printMessage('Ty wygrywasz!');
+      }
+    if( computerMove == 'papier' && playerMove == 'papier'){
+        printMessage('Remis!');
+      }
+    if( computerMove == 'kamien' && playerMove == 'kamien'){
+        printMessage('Remis!');
+      }
+    if( computerMove == 'nozyce' && playerMove == 'nozyce'){
+        printMessage('Remis!');
+      }
+    
+
+    if( playerMove == 'kamień' && computerMove == 'papier'){
+        printMessage('Ty wygrywasz!');
+      }
+    if( playerMove == 'nozyce' && computerMove == 'papier'){
+        printMessage('Ty wygrywasz!');
+      }
+    if( playerMove == 'papier' && computerMove == 'papier'){
+        printMessage('Remis!');
+      }
+    if( playerMove == 'kamien' && computerMovee == 'kamien'){
+        printMessage('Remis!');
+      }
+    if( playerMove == 'nozyce' && computerMove == 'nozyce'){
+        printMessage('Remis!');
+      }
+    
