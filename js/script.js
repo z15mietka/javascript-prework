2282261dcf0b5ae3e1c5f9f75a99666005f9726d
Zaneta Mietka <z.mietka@gmail.com>
2021-07-22 13:55:28 +0200

    Results

--- js/script.js
+++ js/script.js
@@ -11,25 +11,31 @@ if(playerInput == '1'){
     playerMove = 'papier';
 } else if(playerMove == '3'){
     playerMove = 'nozyce';
+}
 
+
+
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  } else if ( computerMove == 'papier' && playerMove == 'kamien'){
+    printMessage('Ty wygrywasz!');
+  } else if ( computerMove == 'nozyce' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
   }
 
-
-
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = 'nieznany ruch';
-
-if(playerInput == '1'){
-  playerMove = 'kamień';
-}else if(playerMove == '2'){
-    playerMove = 'papier';
-} else if(playerMove == '3'){
-    playerMove = 'nozyce';
-
+if( playerMove == 'kamień' && computerMove == 'papier'){
+    printMessage('Ty przegrywasz!');
+  } else if ( playerMove == 'papier' && computerMove == 'kamien'){
+    printMessage('Ty przegrywasz!');
+  } else if ( playerMove == 'nozyce' && computerMove == 'papier'){
+    printMessage('Ty przegrywasz!');
   }
 
-printMessage('Twój ruch to: ' + playerMove);
+  if( computerMove == 'kamień' && playerMove == 'kamien'){
+    printMessage('Remis!');
+  } else if ( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+  } else if ( computerMove == 'nozyce' && playerMove == 'nozyce'){
+    printMessage('Remis!');
+  }
